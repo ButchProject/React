fe49b921cc8c2c2103472b668a629b18ba84c4cb
tuna_window <kdw240@gmail.com>
2023-07-10 06:07:35 +0900

    pretty

--- src/App.js
+++ src/App.js
@@ -11,22 +11,6 @@ function App() {
     <Router>
       <div className="App">
         <CollapsibleExample />
-        <nav>
-          <ul>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">회원가입</Link>
-            </li>
-            <li>
-              <Link to="/chat">채팅</Link>
-            </li>
-            <li>
-              <Link to="/map">맵</Link>
-            </li>
-          </ul>
-        </nav>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
