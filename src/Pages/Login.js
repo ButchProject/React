d01c8507a653ebc7daf5d98999b1e4d0b0734621
kdw240 <kdw240@gmail.com>
2023-07-28 08:02:00 +0900

    add text, small img

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -7,23 +7,40 @@ import '../Styles/Login.css';
 export default function Login() {
   const [visible, setVisible] = useState(false);
 
-  const introStyle = {
+  const Pbus1 = {
     backgroundImage: `url(${process.env.PUBLIC_URL}/image/bus1.png)`
   };
 
-  const rectangle6Style = {
+  const Pbus2 = {
     backgroundImage: `url(${process.env.PUBLIC_URL}/image/bus2.png)`
   };
 
-  const rectangle18Style = {
+  const Pbus3 = {
     backgroundImage: `url(${process.env.PUBLIC_URL}/image/bus3.png)`
   };
 
   return (
     <div className="login-container">
-      <section className="section" id="intro" style={introStyle} />
-      <section className="section" id="rectangle6" style={rectangle6Style} />
-      <section className="section" id="rectangle18" style={rectangle18Style} />
+      <section className="section" id="Pbus1" style={Pbus1} />
+
+      
+      <section className="section with-text image-centered" id="Pbus2" style={Pbus2}>
+        <div className="text-box">
+          <h2>서비스 설명1</h2>
+          <p>설명설명</p>
+          <p>설명설명</p>
+          <p>설명설명</p>
+        </div>
+      </section>
+      
+      <section className="section with-text image-centered" id="Pbus3" style={Pbus3}>
+        <div className="text-box">
+          <h2>서비스 설명2</h2>
+          <p>설명설명</p>
+          <p>설명설명</p>
+          <p>설명설명</p>
+        </div>
+      </section>
 
       <Sidebar visible={visible} onHide={() => setVisible(false)} className="w-full md:w-20rem lg:w-30rem">
         <h2>Login page</h2>
