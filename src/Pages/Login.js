7808d3ee1864546066a6cd8adf553aa0c98ab30c
kdw240 <kdw240@gmail.com>
2023-07-28 08:32:50 +0900

    login sample code

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Sidebar } from 'primereact/sidebar';
 import { Button } from 'primereact/button';
 import { ScrollTop } from 'primereact/scrolltop';
@@ -19,12 +19,35 @@ export default function Login() {
     backgroundImage: `url(${process.env.PUBLIC_URL}/image/bus3.png)`
   };
 
+    // 스크롤 이벤트 처리 함수
+    const handleScroll = () => {
+      const elements = document.querySelectorAll('.scroll-animation');
+      const windowHeight = window.innerHeight;
+  
+      elements.forEach(function (element) {
+        const positionFromTop = element.getBoundingClientRect().top;
+  
+        if (positionFromTop - windowHeight <= 0) {
+          element.classList.add('visible');
+        } else {
+          element.classList.remove('visible');
+        }
+      });
+    };
+  
+    // 스크롤 이벤트 리스너 등록 및 해제
+    useEffect(() => {
+      document.addEventListener('scroll', handleScroll);
+      return () => {
+        document.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+  
   return (
     <div className="login-container">
       <section className="section" id="Pbus1" style={Pbus1} />
 
-      
-      <section className="section with-text image-centered" id="Pbus2" style={Pbus2}>
+      <section className="section with-text image-centered scroll-animation" id="Pbus2" style={Pbus2}>
         <div className="text-box">
           <h2>서비스 설명1</h2>
           <p>설명설명</p>
@@ -32,8 +55,8 @@ export default function Login() {
           <p>설명설명</p>
         </div>
       </section>
-      
-      <section className="section with-text image-centered" id="Pbus3" style={Pbus3}>
+
+      <section className="section with-text image-centered scroll-animation" id="Pbus3" style={Pbus3}>
         <div className="text-box">
           <h2>서비스 설명2</h2>
           <p>설명설명</p>
@@ -43,8 +66,16 @@ export default function Login() {
       </section>
 
       <Sidebar visible={visible} onHide={() => setVisible(false)} className="w-full md:w-20rem lg:w-30rem">
-        <h2>Login page</h2>
-        <p>Login하세요!</p>
+        <form className="login-form">
+          <h2>Login Page</h2>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" autoComplete="off" />
+          <br />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" autoComplete="off" />
+          <br />
+          <Button type="submit" label="Login" onClick={() => setVisible(true)} />
+        </form>
       </Sidebar>
       <div className="sidebar-button-container">
         <Button icon="pi pi-arrow-right" onClick={() => setVisible(true)} style={{ backgroundColor: 'white' }} />
