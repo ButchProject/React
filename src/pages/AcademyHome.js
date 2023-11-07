e7e039c11d70ba4b23043ae73f5c6fbea6ca09ad
kimdongwon <kdw240@gmail.com>
2023-11-07 19:08:06 +0900

    test

--- src/pages/AcademyHome.js
+++ src/pages/AcademyHome.js
@@ -10,7 +10,7 @@ const AcademyHome = () => {
   };
 
   return (
-    <div>
+    <div className="layer">
       <img
         className="logo-black"
         alt="logo-black"
@@ -22,18 +22,72 @@ const AcademyHome = () => {
       </button>
 
       <div className="contents-container">
-        <div className="description-container">서비스 설명</div>
-        <div className="button-container">
-          <button className="button1" onClick={() => goToPage("/login")}>
-            프로필
-          </button>
-          <button className="button2" onClick={() => goToPage("/login")}>
-            게시판
-          </button>
-          <button className="button3" onClick={() => goToPage("/login")}>
-            채팅
-          </button>
+        <div className="description-container">
+          <div className="d1">
+            <h1 className="bigtitle">학원 HOME</h1>
+            <div className="smalltitle">학원을 운영하시나요?</div>
+          </div>
+          <h3 className="d2">
+            학원 간 셔틀버스 노선 공유를 통해 비용을 절약해보세요
+          </h3>
         </div>
+        <button className="button1" onClick={() => goToPage("/login")}>
+          <img
+            className="profilepic"
+            alt="profilepic"
+            src={`${process.env.PUBLIC_URL}/image/profilepic.png`}
+          />
+          <div className="profile-guide">
+            <h2 className="guide1">프로필</h2>
+            <div className="guide2">
+              운영하는 학원에 대한 정보를 업로드할 수 있어요
+            </div>
+            <h4 className="gonow">바로가기</h4>
+            <img
+              className="gopic"
+              alt="gopic"
+              src={`${process.env.PUBLIC_URL}/image/goicon.png`}
+            />
+          </div>
+        </button>
+        <button className="button2" onClick={() => goToPage("/login")}>
+          <img
+            className="boardpic"
+            alt="boardpic"
+            src={`${process.env.PUBLIC_URL}/image/boardpic.png`}
+          />
+          <div className="board-guide">
+            <h2 className="guide1">게시판</h2>
+            <div className="guide2">
+              함께 버스 노선을 공유할 학원을 찾아보세요
+            </div>
+            <h4 className="gonow">바로가기</h4>
+            <img
+              className="gopic"
+              alt="gopic"
+              src={`${process.env.PUBLIC_URL}/image/goicon.png`}
+            />
+          </div>
+        </button>
+        <button className="button3" onClick={() => goToPage("/login")}>
+        <img
+            className="chatpic"
+            alt="chatpic"
+            src={`${process.env.PUBLIC_URL}/image/chatpic.png`}
+          />
+          <div className="chat-guide">
+            <h2 className="guide1">채팅</h2>
+            <div className="guide2">
+              다른 학원들과 채팅을 통해 공유할 노선을 짜보세요
+            </div>
+            <h4 className="gonow">바로가기</h4>
+            <img
+              className="gopic"
+              alt="gopic"
+              src={`${process.env.PUBLIC_URL}/image/goicon.png`}
+            />
+          </div>
+        </button>
       </div>
     </div>
   );
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -18,7 +18,7 @@ const MainPage = () => {
   const clickedImage = `${process.env.PUBLIC_URL}/image/bus-y.png`;
 
   return (
-    <div>
+    <div className="layer">
       <img
         className="logo-black"
         alt="logo-black"
@@ -29,24 +29,74 @@ const MainPage = () => {
         로그아웃
       </button>
 
-      <div className="description-container">서비스 설명</div>
-      <button
-        className={`button1 ${isClicked ? "button1-clicked" : ""} ${isClicked ? "move-left" : ""}`}
-        style={
-          isClicked
-            ? { backgroundImage: `url(${clickedImage})`, opacity: 1 }
-            : {}
-        }
-        onClick={() => goToPage("/profilepage")}
-      >
-        프로필
-      </button>
-      <button className="button2" onClick={() => goToPage("/board")}>
-        게시판
-      </button>
-      <button className="button3" onClick={() => goToPage("/chat")}>
-        채팅
-      </button>
+      <div className="contents-container">
+        <div className="description-container">
+          <div className="d1">
+            <h1 className="bigtitle">학원 HOME</h1>
+            <div className="smalltitle">학원을 운영하시나요?</div>
+          </div>
+          <h3 className="d2">
+            학원 간 셔틀버스 노선 공유를 통해 비용을 절약해보세요
+          </h3>
+        </div>
+        <button className="button1" onClick={() => goToPage("/profilepage")}>
+          <img
+            className="profilepic"
+            alt="profilepic"
+            src={`${process.env.PUBLIC_URL}/image/profilepic.png`}
+          />
+          <div className="profile-guide">
+            <h2 className="guide1">프로필</h2>
+            <div className="guide2">
+              운영하는 학원에 대한 정보를 업로드할 수 있어요
+            </div>
+            <h4 className="gonow">바로가기</h4>
+            <img
+              className="gopic"
+              alt="gopic"
+              src={`${process.env.PUBLIC_URL}/image/goicon.png`}
+            />
+          </div>
+        </button>
+        <button className="button2" onClick={() => goToPage("/board")}>
+          <img
+            className="boardpic"
+            alt="boardpic"
+            src={`${process.env.PUBLIC_URL}/image/boardpic.png`}
+          />
+          <div className="board-guide">
+            <h2 className="guide1">게시판</h2>
+            <div className="guide2">
+              함께 버스 노선을 공유할 학원을 찾아보세요
+            </div>
+            <h4 className="gonow">바로가기</h4>
+            <img
+              className="gopic"
+              alt="gopic"
+              src={`${process.env.PUBLIC_URL}/image/goicon.png`}
+            />
+          </div>
+        </button>
+        <button className="button3" onClick={() => goToPage("/chat")}>
+        <img
+            className="chatpic"
+            alt="chatpic"
+            src={`${process.env.PUBLIC_URL}/image/chatpic.png`}
+          />
+          <div className="chat-guide">
+            <h2 className="guide1">채팅</h2>
+            <div className="guide2">
+              다른 학원들과 채팅을 통해 공유할 노선을 짜보세요
+            </div>
+            <h4 className="gonow">바로가기</h4>
+            <img
+              className="gopic"
+              alt="gopic"
+              src={`${process.env.PUBLIC_URL}/image/goicon.png`}
+            />
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
